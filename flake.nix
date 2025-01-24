4ccb867a9b374018a172393a3df9246950d4c15a
Akshay <nerdy@peppe.rs>
2025-01-24 23:01:00 +0000

    add rust bits to nix config

--- flake.nix
+++ flake.nix
@@ -1,31 +1,53 @@
 {
   description = "atproto github";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    {
-      defaultPackage = forAllSystems (system: self.packages.${system}.legit);
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              go
-              air
-            ];
-          };
-        });
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    gitignore,
+    rust-overlay,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      });
+  in {
+    defaultPackage = forAllSystems (system: self.packages.${system}.legit);
+    formatter = forAllSystems (system: nixpkgsFor."${system}".alejandra);
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      rust-bin = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    in {
+      default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.go
+          pkgs.air
+
+          pkgs.bacon
+          rust-bin
+          pkgs.pkg-config
+          pkgs.openssl
+        ];
+        RUST_LOG = "info";
+        RUST_BACKTRACE = 1;
+      };
+    });
+  };
 }
