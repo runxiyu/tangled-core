e5a13e80044d786c446561377e13d3a4ba891fc2
Akshay <nerdy@peppe.rs>
2025-01-26 18:23:06 +0000

    implement GET /keys and PUT /keys

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,20 +1,22 @@
 use std::sync::Arc;
 
-use atrium_api::{
-    agent::{AtpAgent, store::MemorySessionStore},
-    did_doc::DidDocument,
-    types::string::AtIdentifier,
-};
+use atrium_api::{did_doc::DidDocument, types::string::AtIdentifier};
 use atrium_common::resolver::Resolver;
 use atrium_identity::{
     did::{CommonDidResolver, CommonDidResolverConfig, DEFAULT_PLC_DIRECTORY_URL},
-    handle::{AtprotoHandleResolver, AtprotoHandleResolverConfig, DnsTxtResolver},
+    handle::{AtprotoHandleResolver, AtprotoHandleResolverConfig},
 };
 use atrium_oauth_client::DefaultHttpClient;
 use atrium_xrpc::HttpClient;
-use atrium_xrpc_client::isahc::IsahcClient;
-use axum::{Router, routing};
-use hickory_resolver::TokioAsyncResolver;
+use axum::{Extension, Router, routing};
+use tokio_rusqlite::Connection;
+
+mod db;
+mod dns;
+mod routes;
+
+use db::Db;
+use routes::{index, keys, login};
 
 #[tokio::main]
 async fn main() {
@@ -22,15 +24,17 @@ async fn main() {
     let session_layer = tower_sessions::SessionManagerLayer::new(session_store)
         .with_secure(false)
         .with_expiry(tower_sessions::Expiry::OnSessionEnd);
+    let db = Db::new(Connection::open("bild.db").await.unwrap());
+    db.setup().await;
 
     let app_state = AppState::new();
     let service = Router::new()
         .route("/", routing::get(index::get))
-        // .route("/test-auth", routing::get(test_atproto::get))
         .route("/login", routing::get(login::get).post(login::post))
-        // .route("/callback", routing::get(callback::get))
+        .route("/keys", routing::get(keys::get).put(keys::put))
         .layer(session_layer)
-        .with_state(app_state);
+        .with_state(app_state)
+        .layer(Extension(Arc::new(db)));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, service).await.unwrap();
@@ -39,7 +43,7 @@ async fn main() {
 #[derive(Clone)]
 struct AppState {
     did_resolver: Arc<CommonDidResolver<DefaultHttpClient>>,
-    handle_resolver: Arc<AtprotoHandleResolver<HickoryDnsTxtResolver, DefaultHttpClient>>,
+    handle_resolver: Arc<AtprotoHandleResolver<dns::Resolver, DefaultHttpClient>>,
 }
 
 impl AppState {
@@ -72,92 +76,9 @@ fn did_resolver<H: HttpClient>(http_client: Arc<H>) -> CommonDidResolver<H> {
     })
 }
 
-fn handle_resolver<H: HttpClient>(
-    http_client: Arc<H>,
-) -> AtprotoHandleResolver<HickoryDnsTxtResolver, H> {
+fn handle_resolver<H: HttpClient>(http_client: Arc<H>) -> AtprotoHandleResolver<dns::Resolver, H> {
     AtprotoHandleResolver::new(AtprotoHandleResolverConfig {
-        dns_txt_resolver: HickoryDnsTxtResolver::default(),
+        dns_txt_resolver: dns::Resolver::default(),
         http_client,
     })
-}
-
-mod login {
-    use axum::{extract::Form, http::StatusCode, response::IntoResponse};
-    use serde::Deserialize;
-
-    use super::*;
-
-    pub async fn get() -> String {
-        "hello world".to_owned()
-    }
-
-    #[derive(Deserialize, Debug)]
-    pub struct Req {
-        handle: AtIdentifier,
-        app_password: String,
-    }
-
-    pub async fn post(session: tower_sessions::Session, Form(req): Form<Req>) -> impl IntoResponse {
-        let agent = AtpAgent::new(
-            IsahcClient::new("https://dummy.example"),
-            MemorySessionStore::default(),
-        );
-        let res = agent.login(req.handle, req.app_password).await.unwrap();
-        println!("logged in as {:?} ({:?})", res.handle, res.did);
-        session.insert("at_session", res).await.unwrap();
-        println!("stored session");
-        StatusCode::OK
-    }
-}
-
-// dummy endpoint to test if sessions are working
-mod index {
-    use super::*;
-
-    pub async fn get(session: tower_sessions::Session) -> &'static str {
-        match session
-            .get::<atrium_api::agent::Session>("at_session")
-            .await
-            .unwrap()
-        {
-            None => "no session",
-            Some(s) => {
-                let agent = AtpAgent::new(
-                    IsahcClient::new("https://dummy.example"),
-                    MemorySessionStore::default(),
-                );
-                println!("resuming session of {:?} ({:?})", s.handle, s.did);
-                agent.resume_session(s).await.unwrap();
-                "resuming session"
-            }
-        }
-    }
-}
-
-struct HickoryDnsTxtResolver {
-    resolver: TokioAsyncResolver,
-}
-
-impl Default for HickoryDnsTxtResolver {
-    fn default() -> Self {
-        Self {
-            resolver: TokioAsyncResolver::tokio_from_system_conf()
-                .expect("failed to create resolver"),
-        }
-    }
-}
-
-impl DnsTxtResolver for HickoryDnsTxtResolver {
-    async fn resolve(
-        &self,
-        query: &str,
-    ) -> core::result::Result<Vec<String>, Box<dyn std::error::Error + Send + Sync + 'static>> {
-        Ok(self
-            .resolver
-            .txt_lookup(query)
-            .await?
-            .iter()
-            .map(|txt| txt.to_string())
-            .collect())
-    }
 }
---
+++ core/src/routes.rs
@@ -0,0 +1,135 @@
+pub mod login {
+    use atrium_api::{
+        agent::{AtpAgent, store::MemorySessionStore},
+        types::string::AtIdentifier,
+    };
+    use atrium_xrpc_client::isahc::IsahcClient;
+    use axum::{extract::Form, http::StatusCode, response::IntoResponse};
+    use serde::Deserialize;
+
+    pub async fn get() -> String {
+        "hello world".to_owned()
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct Req {
+        handle: AtIdentifier,
+        app_password: String,
+    }
+
+    pub async fn post(session: tower_sessions::Session, Form(req): Form<Req>) -> impl IntoResponse {
+        let agent = AtpAgent::new(
+            IsahcClient::new("https://dummy.example"),
+            MemorySessionStore::default(),
+        );
+        let res = agent.login(req.handle, req.app_password).await.unwrap();
+        println!("logged in as {:?} ({:?})", res.handle, res.did);
+        session.insert("bild_session", res).await.unwrap();
+        println!("stored session");
+        StatusCode::OK
+    }
+}
+
+pub mod index {
+    use atrium_api::agent::{AtpAgent, store::MemorySessionStore};
+    use atrium_xrpc_client::isahc::IsahcClient;
+
+    pub async fn get(session: tower_sessions::Session) -> &'static str {
+        match session
+            .get::<atrium_api::agent::Session>("bild_session")
+            .await
+            .unwrap()
+        {
+            None => "no session",
+            Some(s) => {
+                let agent = AtpAgent::new(
+                    IsahcClient::new("https://dummy.example"),
+                    MemorySessionStore::default(),
+                );
+                println!("resuming session of {:?} ({:?})", s.handle, s.did);
+                agent.resume_session(s).await.unwrap();
+                "resuming session"
+            }
+        }
+    }
+}
+
+pub mod keys {
+    use crate::{AppState, db};
+    use atrium_api::types::string::AtIdentifier;
+    use axum::http::StatusCode;
+    use axum::{
+        extract::{Extension, Json, Query, State},
+        response::IntoResponse,
+    };
+    use serde::{Deserialize, Serialize};
+    use std::sync::Arc;
+
+    #[derive(Deserialize)]
+    pub struct GetReq {
+        id: AtIdentifier,
+    }
+
+    #[derive(Serialize)]
+    pub struct GetRes {
+        id: AtIdentifier,
+        keys: Vec<String>,
+    }
+
+    pub async fn get(
+        Extension(db): Extension<Arc<db::Db>>,
+        State(state): State<AppState>,
+        Query(req): Query<GetReq>,
+    ) -> impl IntoResponse {
+        let did_doc = state.resolve_did_document(&req.id).await.unwrap();
+        let keys = db
+            .conn
+            .call(|c| {
+                let mut stmt = c.prepare("SELECT key FROM keys WHERE did = ?1")?;
+                let keys = stmt
+                    .query_map([did_doc.id], |row| Ok(row.get::<usize, String>(0)?))?
+                    .filter_map(|r| r.ok())
+                    .collect::<Vec<_>>();
+                Ok(keys)
+            })
+            .await
+            .unwrap();
+        Json(GetRes {
+            id: req.id.clone(),
+            keys,
+        })
+    }
+
+    #[derive(Deserialize)]
+    pub struct PutReq {
+        key: String,
+    }
+
+    pub async fn put(
+        Extension(db): Extension<Arc<db::Db>>,
+        session: tower_sessions::Session,
+        Json(req): Json<PutReq>,
+    ) -> impl IntoResponse {
+        if openssh_keys::PublicKey::parse(&req.key).is_err() {
+            return StatusCode::BAD_REQUEST;
+        }
+        match session
+            .get::<atrium_api::agent::Session>("bild_session")
+            .await
+            .unwrap()
+        {
+            None => StatusCode::UNAUTHORIZED,
+            Some(sess) => {
+                let did = sess.did.clone().into();
+                db.conn
+                    .call(|c| {
+                        c.execute("INSERT INTO keys VALUES (?1, ?2)", [did, req.key])?;
+                        Ok(())
+                    })
+                    .await
+                    .unwrap();
+                StatusCode::OK
+            }
+        }
+    }
+}
